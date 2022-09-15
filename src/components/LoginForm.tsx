86adfd232ed3826fb4725db0b22d5201a40ad496
yoon <yunyun40550082@gmail.com>
2022-09-16 01:15:56 +0900

    feat: 아이디, 비밀번호 입력 값 유효성 검사 후 로그인 버튼 활성화 기능 구현

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,18 +1,70 @@
 import { Input } from '@/components';
 import styled from '@emotion/styled';
-import { useEffect, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { vaildateId, vaildatePassword } from '@/utilities/formValidate';
 
-type ErrorsType = { [key: string]: string } | null;
+type ValuesType = { [key: string]: string };
+
+const DEFAULT_VALUE: ValuesType = { id: '', password: '' };
+const INPUT_LENGTH = Object.keys(DEFAULT_VALUE).length;
 
 const LoginForm = () => {
-  const [values, setValues] = useState({ id: '', password: '' });
-  const [errors, setErrors] = useState<ErrorsType>({ id: '', password: '' });
+  const [values, setValues] = useState(DEFAULT_VALUE);
+  const [errorsState, setErrorsState] = useState(DEFAULT_VALUE);
+  const errors = useRef<ValuesType>({});
 
-  const handleBlur = (e: any) => {};
+  const handleFocusEvent = (event: React.FocusEvent<HTMLInputElement>) => {
+    const { name } = event.target;
+    setErrorsState({ ...errorsState, [name]: errors.current[name] });
+  };
+
+  const idValidate = (id: string) => {
+    if (!vaildateId(id)) {
+      errors.current.id = '올바른 아이디 형식으로 입력해주세요.';
+      return;
+    }
+    errors.current.id = '';
+    setErrorsState({ ...errorsState, id: '' });
+  };
+
+  const passwordValidate = (password: string) => {
+    if (!vaildatePassword(password)) {
+      errors.current.password = '올바른 비밀번호 형식으로 입력해주세요.';
+      return;
+    }
+    errors.current.password = '';
+    setErrorsState({ ...errorsState, password: '' });
+  };
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setValues({ ...values, [name]: value.trim() });
+
+    if (name === 'id') {
+      idValidate(value.trim());
+    }
+
+    if (name === 'password') {
+      passwordValidate(value.trim());
+    }
+  };
+
+  const checkLoginActive = () => {
+    const errorsLength = Object.keys(errors.current).length;
+
+    // 입력이 전부 안됐을 때
+    if (errorsLength < INPUT_LENGTH) {
+      return false;
+    }
+
+    // 입력이 됐지만 유효할 때
+    for (const key in errors.current) {
+      if (errors.current[key]) {
+        return false;
+      }
+    }
+
+    return true;
   };
 
   return (
@@ -22,20 +74,20 @@ const LoginForm = () => {
         label='아이디'
         type='text'
         value={values.id}
-        errorMessage={errors?.id}
+        errorMessage={errorsState?.id}
         onChange={handleChange}
-        onBlur={handleBlur}
+        onBlur={handleFocusEvent}
       />
       <PasswordInput
         name='password'
         label='비밀번호'
         type='password'
         value={values.password}
-        errorMessage={errors?.password}
+        errorMessage={errorsState?.password}
         onChange={handleChange}
-        onBlur={handleBlur}
+        onBlur={handleFocusEvent}
       />
-      <LoginButton disabled>로그인</LoginButton>
+      <LoginButton disabled={checkLoginActive() ? false : true}>로그인</LoginButton>
     </Form>
   );
 };
