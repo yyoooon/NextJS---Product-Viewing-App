75a61806fd5c9dd9d36fd5dd97071315cbcc40ef
yoon <yunyun40550082@gmail.com>
2022-09-20 23:23:43 +0900

    refactor: observer target을 state에서 ref로 변경

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,4 @@
 export { default as useUser } from './useUser';
 export { default as usePagination } from './usePagination';
 export { default as useProducts } from './useProducts';
-export { default as useIntersection } from './useIntersection';
+export { default as useIntersect } from './useIntersect';
