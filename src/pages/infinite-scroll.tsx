bf954b7c4a1c874e2b655375ac5645a45bf0912d
yoon <yunyun40550082@gmail.com>
2022-09-20 18:56:33 +0900

    feat: intersection observer로직 작성

--- src/pages/infinite-scroll.tsx
+++ src/pages/infinite-scroll.tsx
@@ -1,16 +1,35 @@
 import type { NextPage } from 'next';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 
 import products from '../api/data/products.json';
 import ProductList from '@/components/ProductList/ProductList';
 
 const InfiniteScrollPage: NextPage = () => {
+  const [target, setTarget] = useState<HTMLElement | null>(null);
+
+  const onIntersect: IntersectionObserverCallback = ([entry], observer) => {
+    if (entry.isIntersecting) {
+      observer.unobserve(entry.target);
+      // 데이터 불러오는 로직 작성
+    }
+  };
+
+  useEffect(() => {
+    if (!target) return;
+
+    const observer = new IntersectionObserver(onIntersect, { threshold: 0.5 });
+    observer.observe(target);
+
+    return () => observer.disconnect();
+  }, [target]);
+
   return (
     <>
       <Container>
         <ProductList products={products} />
       </Container>
+      <div ref={setTarget}></div>
     </>
   );
 };
