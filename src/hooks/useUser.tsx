930b6ea9056bf9551c9312489c35593ffe48432d
yoon <yunyun40550082@gmail.com>
2022-09-22 00:23:53 +0900

    chore: 불필요한 코드 제거

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,28 +1,26 @@
+import { useSetRecoilState } from 'recoil';
+
 import { signIn } from '@/api/auth';
 import { TOKEN_NAME } from '@/constants';
 import { userState } from '@/states/user';
-import { useState } from 'react';
-import { useRecoilState } from 'recoil';
 
 const useUser = () => {
-  const [userInfo, setUserInfo] = useRecoilState(userState);
-  const [isLogin, setIsLogin] = useState(userInfo ? true : false);
+  const setUserInfo = useSetRecoilState(userState);
 
   const login = async (id: string, password: string) => {
     const { data } = await signIn({ id, password });
     const { accessToken, user } = data.data;
+
     sessionStorage.setItem(TOKEN_NAME, JSON.stringify(accessToken));
     setUserInfo({ id: user.ID, name: user.NAME });
-    setIsLogin(true);
   };
 
   const logout = () => {
     sessionStorage.removeItem(TOKEN_NAME);
     setUserInfo(null);
-    setIsLogin(false);
   };
 
-  return { isLogin, login, logout };
+  return { login, logout };
 };
 
 export default useUser;
--- src/states/user.ts
+++ src/states/user.ts
@@ -1,4 +1,4 @@
-import { atom, selector } from 'recoil';
+import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 import { UserInfoType } from '@/types';
 
@@ -11,11 +11,4 @@ export const userState = atom<UserInfoType | null>({
   key: 'userState',
   default: null,
   effects_UNSTABLE: [persistAtom],
-});
-
-export const userStateSelector = selector({
-  key: 'userStateSelector',
-  get: async ({ get }) => {
-    // 유저 정보 api 호출
-  },
 });
